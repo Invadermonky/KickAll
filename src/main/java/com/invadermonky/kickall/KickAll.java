cab9043093ee61bd25715119c05422cd5d9e4f8c
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2024-05-30 10:47:38 -0700

    Initial commit

---
+++ src/main/java/com/invadermonky/kickall/KickAll.java
@@ -0,0 +1,25 @@
+package com.invadermonky.kickall;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(
+        modid = KickAll.MOD_ID,
+        name = KickAll.MOD_NAME,
+        version = KickAll.MOD_VERSION,
+        acceptedMinecraftVersions = KickAll.MC_VERSIONS
+)
+public class KickAll {
+    public static final String MOD_ID = "kickall";
+    public static final String MOD_NAME = "KickAll";
+    public static final String MOD_VERSION = "1.12.2-1.0.0";
+    public static final String MC_VERSIONS = "[1.12.2]";
+
+    @Mod.Instance
+    public static KickAll INSTANCE;
+
+    @Mod.EventHandler
+    public void serverStarting(FMLServerStartingEvent event) {
+        event.registerServerCommand(new KickAllCommand());
+    }
+}
---
+++ src/main/java/com/invadermonky/kickall/KickAllCommand.java
@@ -0,0 +1,49 @@
+package com.invadermonky.kickall;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.SERVER)
+public class KickAllCommand extends CommandBase {
+    @Override
+    public String getName() {
+        return "kickall";
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender) {
+        return "/kickall [message]";
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        if(server.isSinglePlayer()) {
+            throw new WrongUsageException("Cannot use command in single player worlds.");
+        }
+
+        String[] onlinePlayers = server.getOnlinePlayerNames();
+        StringBuilder message = new StringBuilder();
+
+        for(String arg : args) {
+            message.append(" ").append(arg);
+        }
+
+        for(String playerName : onlinePlayers) {
+            if(playerName.equals(sender.getName())) {
+                continue;
+            }
+            String command = "kick " + playerName + message;
+            server.commandManager.executeCommand(sender, command);
+        }
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 3;
+    }
+}
